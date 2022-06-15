b879cbcefc81238ae9cb9217143f8e5e84a42c64
Juan <thecatbox@protonmail.com>
2022-06-15 12:26:49 -0700

    card title fix

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -20,11 +20,11 @@ export default function PostCard({
         </div>
         <div className="card-body">
           <p className="card-text text-secondary">{category}</p>
-          <Link href={`/posts/${slug}`} passHref><h5 className="card-title">{title}</h5></Link>
+          <Link href={`/posts/${slug}`} passHref><a style={{ textDecoration: 'none', color: 'black' }}><h5 className="card-title">{title}</h5></a></Link>
           <p className="card-text">{description}</p>
           <section style={{ display: 'flex', justifyContent: 'space-between' }}>
             <div>
-              <Link href={`/posts/${slug}`} ><a style={{ textDecoration: 'none' }}>Leer mas</a></Link>
+              <Link href={`/posts/${slug}`} passHref><a style={{ textDecoration: 'none' }}>Leer mas</a></Link>
             </div>
             <div style={{ color: 'grey' }}>
               <Date dateString={publishedAt}/>{` | ${readingTime.text}`}
